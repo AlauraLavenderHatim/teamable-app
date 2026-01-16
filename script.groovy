10925dd93a8ef406e0899333feea756da3eafa83
AlauraLavenderHatim <lavender.alaura@gmail.com>
2026-01-16 19:42:00 +0100

    Update groovy script

--- script.groovy
+++ script.groovy
@@ -1,5 +1,9 @@
 def buildApp() {
     echo 'building the application...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                        sh 'docker build -t alavenderhat/teamable-app:tma-1.0 .'
+                        sh 'echo $PASS | docker login -u $USER --password-stdin'
+                        sh 'docker push alavenderhat/teamable-ap
 }
 
 def testApp() {
