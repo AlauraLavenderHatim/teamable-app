c3128bb02f0fa123944fcdd25eec3a41e6759288
AlauraLavenderHatim <lavender.alaura@gmail.com>
2026-01-16 19:44:41 +0100

    Fix groovy script typo

--- script.groovy
+++ script.groovy
@@ -3,7 +3,7 @@ def buildApp() {
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
                         sh 'docker build -t alavenderhat/teamable-app:tma-1.0 .'
                         sh 'echo $PASS | docker login -u $USER --password-stdin'
-                        sh 'docker push alavenderhat/teamable-ap
+                        sh 'docker push alavenderhat/teamable-app:tma-1.0'
 }
 
 def testApp() {
